06f7e461f1850a57cab87be70ffcc941e3866fac
Vigneshk5 <vigneshpatali5@gmail.com>
2025-02-25 15:11:14 +0530

    latest

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/vigneshk/Desktop/work/TwpDocBot/src/app/layout":["static/media/c22ccc5eb58b83e1-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
